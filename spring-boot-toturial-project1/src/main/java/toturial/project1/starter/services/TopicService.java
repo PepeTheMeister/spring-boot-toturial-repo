ebc6e60507132505e20be047a8286e1588d7eeea
luis_mauricio <luisfcmauricio@gmail.com>
2022-02-17 15:09:30 +0000

    finalização primeiro projeto
    Primeiro projeto de toturial de spring boot finalizado com implementação dos serviços para os pedidos PUT, DELETE

--- spring-boot-toturial-project1/src/main/java/toturial/project1/starter/services/TopicService.java
+++ spring-boot-toturial-project1/src/main/java/toturial/project1/starter/services/TopicService.java
@@ -31,4 +31,16 @@ public class TopicService {
 	public void addTopic(Topic topic) {
 		topics.add(topic);
 	}
+
+	public void updateTopic(Topic topic, String id) {
+		for(int i = 0; i <topics.size(); i++) {
+			if(topics.get(i).getId().equals(id)) {
+				topics.set(i, topic);
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
 }
