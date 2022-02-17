311fa33c17edf46f758921fafbefd0e69248fe87
luis_mauricio <luisfcmauricio@gmail.com>
2022-02-17 15:10:41 +0000

    Criação do segundo projeto
    Criação do projeto2 com o spring initializer

---
+++ spring-boot-initializer/src/test/java/io/toturial/project2/springbootinitializer/SpringBootInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.toturial.project2.springbootinitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
